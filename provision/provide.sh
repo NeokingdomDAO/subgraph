c7f89ecaa196f394daab0755b78e05a232f0f063
Alberto Granzotto <alberto@granzotto.net>
2023-10-20 17:20:09 +0200

    Add cors

--- provision/provide.sh
+++ provision/provide.sh
@@ -101,18 +101,51 @@ install_node() {
 install_configure_nginx() {
     apt install -y nginx
 
+    cat >/etc/nginx/snippets/cors-config.conf <<EOF
+if (\$request_method = 'OPTIONS') {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+    #
+    # Custom headers and headers various browsers *should* be OK with but aren't
+    #
+    add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+    #
+    # Tell client that this pre-flight info is valid for 20 days
+    #
+    add_header 'Access-Control-Max-Age' 1728000;
+    add_header 'Content-Type' 'text/plain; charset=utf-8';
+    add_header 'Content-Length' 0;
+    return 204;
+}
+if (\$request_method = 'POST') {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+    add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+    add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+}
+if (\$request_method = 'GET') {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+    add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+    add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+}
+
+EOF
+
     # Embed the server configuration with the correct domain
     cat >/etc/nginx/sites-available/$DOMAIN <<EOF
 server {
     server_name $DOMAIN;
 
     location /ipfs/api/v0/add {
+        include /etc/nginx/snippets/cors-config.conf;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_pass http://127.0.0.1:5001/api/v0/add;
     }
 
     location /ipfs/api/v0/pin/add {
+        include /etc/nginx/snippets/cors-config.conf;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_pass http://127.0.0.1:5001/api/v0/pin/add;
